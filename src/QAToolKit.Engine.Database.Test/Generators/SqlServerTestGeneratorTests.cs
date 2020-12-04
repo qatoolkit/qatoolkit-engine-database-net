e29d9d354fd5e7cd784f63cf66f004826c8db4eb
Miha Jakovac <miha@mjc.si>
2020-12-04 18:53:25 +0100

    tests updates with ci pipeline fix

--- src/QAToolKit.Engine.Database.Test/Generators/SqlServerTestGeneratorTests.cs
+++ src/QAToolKit.Engine.Database.Test/Generators/SqlServerTestGeneratorTests.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace QAToolKit.Engine.Database.Test
+namespace QAToolKit.Engine.Database.Test.Generators
 {
     public class SqlServerTestGeneratorTests
     {
---
+++ src/QAToolKit.Engine.Database.Test/Runners/SqlServerTestGeneratorTests.cs
@@ -0,0 +1,31 @@
+﻿using ExpectedObjects;
+using NSubstitute;
+using QAToolKit.Engine.Database.Generators;
+using QAToolKit.Engine.Database.Models;
+using QAToolKit.Engine.Database.Runners;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QAToolKit.Engine.Database.Test.Runners
+{
+    public class SqlServerTestRunnerTests
+    {
+        [Fact]
+        public async Task MysqlCreateRunnerTest_Success()
+        {
+            var generator = new SqlServerTestGenerator(options =>
+            {
+                options.AddDatabaseObjectExitsRule(new string[] { "mytable" }, DatabaseObjectType.Table);
+            });
+
+            var script = await generator.Generate();
+
+            var options = Substitute.For<Action<DatabaseTestRunnerOptions>>();
+            var runner = Substitute.For<SqlServerTestRunner>(script, options);
+
+            Assert.NotNull(runner);
+        }
+    }
+}
